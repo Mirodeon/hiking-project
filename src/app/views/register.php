d11920b5cf98243ec977cecaf94627ebc2ba7483
unknown <luuduc34@hotmail.com>
2022-07-25 16:38:10 +0200

    fix script js, add file js menu buerger, link js file to link

--- src/app/views/register.php
+++ src/app/views/register.php
@@ -1,4 +1,4 @@
-<?php $title = "Login"; ?>
+<?php $title = "register"; ?>
 <?php require "parts/head.php"; ?>
 <?php include 'header.php'; ?>
 <div class="hero is-primary">
@@ -6,25 +6,25 @@
         <div class="container">
             <div class="columns is-centered">
                 <form method="post" class="box">
-                <div class="field">
+                    <div class="field">
                         <label for="firstname" class="label">Firstname</label>
                     </div>
                     <div class="control has-icons-left">
                         <input type="text" class="input" placeholder="Your firstname">
                         <span class="icon is-small is-left">
-                            <i class="fa fa-name"></i>
+                            <i class="fa fa-user-o"></i>
                         </span>
                     </div>
-                <div class="field">
+                    <div class="field">
                         <label for="lastname" class="label">Lastname</label>
                     </div>
                     <div class="control has-icons-left">
                         <input type="text" class="input" placeholder="Your lastname">
                         <span class="icon is-small is-left">
-                            <i class="fa fa-name"></i>
+                            <i class="fa fa-user-o"></i>
                         </span>
                     </div>
-                <div class="field">
+                    <div class="field">
                         <label for="email" class="label">Email</label>
                     </div>
                     <div class="control has-icons-left">

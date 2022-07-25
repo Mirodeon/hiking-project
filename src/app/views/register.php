78daa26df966dcee04d8d0a7e42536361923eab0
Mirodeon <mehdi.habaru@gmail.com>
2022-07-25 18:12:22 +0200

    delete merge statement in vs

--- src/app/views/register.php
+++ src/app/views/register.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <?php $title = "Register"; ?>
-=======
-<?php $title = "register"; ?>
->>>>>>> d11920b5cf98243ec977cecaf94627ebc2ba7483
 <?php require "parts/head.php"; ?>
 <?php include 'header.php'; ?>
 <div class="hero is-primary">

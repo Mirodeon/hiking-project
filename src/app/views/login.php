8a372c7972bcea0a1dee7bd1a1837d365ceeaa92
Mirodeon <mehdi.habaru@gmail.com>
2022-07-25 04:12:03 +0200

    add parts head and rearrange title, head, header, footer

--- src/app/views/login.php
+++ src/app/views/login.php
@@ -3,11 +3,13 @@
 // open the $_SESSION
 session_start();
 
-if(!empty($_POST)) {
+if (!empty($_POST)) {
     // 1. Check all the inputs exist
     // 2. We check also if the $_POST are not empty because we load the page, the form is empty
-    if(isset($_POST["login"],$_POST["pass"])
-        && !empty($_POST["login"]) && !empty($_POST["pass"])) {
+    if (
+        isset($_POST["login"], $_POST["pass"])
+        && !empty($_POST["login"]) && !empty($_POST["pass"])
+    ) {
 
         $login = strip_tags($_POST["login"]);
 
@@ -19,18 +21,18 @@ if(!empty($_POST)) {
         $q->bindParam(":login", $login, PDO::PARAM_STR);
 
         // exexute return a boolean
-        if(!$q->execute()) {
+        if (!$q->execute()) {
             die("form not sent to the db");
         }
 
         $user = $q->fetch(PDO::FETCH_ASSOC);
 
-        if(!$user) {
+        if (!$user) {
             die("user doesn't exist &/or password incorrect");
         }
 
         // check the password input with the password in db
-        if(!password_verify($_POST["pass"], $user["password"])){
+        if (!password_verify($_POST["pass"], $user["password"])) {
             die("user doesn't exist &/or password incorrect");
         }
 
@@ -43,56 +45,43 @@ if(!empty($_POST)) {
         ];
 
         header("location: index.php");
-
     }
 }
 
-include "header.php";
-
 ?>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <script src="https://kit.fontawesome.com/de0135f311.js" crossorigin="anonymous"></script> 
-</head>
-
-<body>
-    <div class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-centered">
-                    <form method="post" class="box">
-                        <div class="field">
-                            <label for="login" class="label">Login</label>
-                        </div>
-                        <div class="control has-icons-left">
-                            <input type="text" class="input" placeholder="Your login">
-                            <span class="icon is-small is-left">
-                                <i class="fa fa-sign-in"></i>
-                            </span>
-                        </div>
-                        <div class="field">
-                            <label for="pass" class="label">Password</label>
-                        </div>
-                        <div class="control has-icons-left">
-                            <input type="text" class="input" placeholder="Your password">
-                            <span class="icon is-small is-left">
-                                <i class="fa fa-lock"></i>
-                            </span>
-                        </div>
-                        <div class="field">
-                            <button class="button is-success">Login</button>
-                        </div>
-                    </form>
-                </div>
+<?php $title = "Login"; ?>
+<?php require "parts/head.php"; ?>
+<script src="https://kit.fontawesome.com/de0135f311.js" crossorigin="anonymous"></script>
+<?php include 'header.php'; ?>
+<div class="hero is-primary">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns is-centered">
+                <form method="post" class="box">
+                    <div class="field">
+                        <label for="login" class="label">Login</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="text" class="input" placeholder="Your login">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-sign-in"></i>
+                        </span>
+                    </div>
+                    <div class="field">
+                        <label for="pass" class="label">Password</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="text" class="input" placeholder="Your password">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                    </div>
+                    <div class="field">
+                        <button class="button is-success">Login</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</body>
-
-<?php
-include "footer.php";
-?>
+</div>
+<?php include "footer.php"; ?>

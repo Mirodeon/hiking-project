55b0bc39e4076d5158cd39ab4f0bd2bd265488c3
Mirodeon <mehdi.habaru@gmail.com>
2022-07-25 04:18:57 +0200

    add icone tab

--- src/app/views/parts/head.php
+++ src/app/views/parts/head.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= (isset($title)) ? $title : "Page Doesn't exist" ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="icon" type="image/x-icon" href="./img/rando-icone.png">
   </head>
   <body>

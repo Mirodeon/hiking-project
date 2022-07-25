0b25e37c2bb9cbfc3a2faae705aaa5aa40ada2b2
Mirodeon <mehdi.habaru@gmail.com>
2022-07-25 02:59:01 +0200

    add pdo, connect to db and settings ini

--- src/public/index.php
+++ src/public/index.php
@@ -2,9 +2,19 @@
 require_once 'core/Request.php';
 require_once 'core/Router.php';
 require_once 'app/routes.php';
+require_once 'app/models/MyPDO.php';
 $uri = Request::uri();
 $method = Request::method();
 $router = new Router();
 $router->register($routes);
 $router->direct($uri, $method);
+try
+{
+	$db = new MyPDO();
+    echo 'connected to db !';
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
 ?>

beb33c9d427deeae06ab3f721870447de59a2405
unknown <luuduc34@hotmail.com>
2022-07-25 09:26:01 +0200

    mehdi

--- src/app/views/login.php
+++ src/app/views/login.php
@@ -54,7 +54,7 @@ include "header.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
+    <title>The Hiking Project</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <script src="https://kit.fontawesome.com/de0135f311.js" crossorigin="anonymous"></script> 
 </head>
@@ -82,7 +82,7 @@ include "header.php";
                             <span class="icon is-small is-left">
                                 <i class="fa fa-lock"></i>
                             </span>
-                        </div>
+                        </div></br>
                         <div class="field">
                             <button class="button is-success">Login</button>
                         </div>

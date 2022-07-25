b30dd8dddfa6e77b38f82383019e9e75fce63171
Mirodeon <mehdi.habaru@gmail.com>
2022-07-25 02:21:17 +0200

    Up

--- src/app/routes.php
+++ src/app/routes.php
@@ -3,12 +3,9 @@ $routes = [
     'GET' => [
         '/' => 'app/views/home.php',
         '/home' => 'app/views/home.php',
+        '' => 'app/views/home.php',
         '/404' => 'app/views/404.php',
         '/login' => 'app/views/login.php',
-<<<<<<< HEAD
-=======
-        /*'/ressources/style.css' => 'app/views/ressources/style.css',*/
->>>>>>> 0c2a2981ccfcf29f62b65f02ee5bdf64db70fa1b
     ],
     'POST' => [
 
--- src/app/views/header.php
+++ src/app/views/header.php
@@ -11,11 +11,7 @@
           <strong>HIKING PROJECT</strong>
         </p>
       <p class="level-item has-text-centered">
-<<<<<<< HEAD
         <a class="link is-info" href="login" title="login">Login</a>
-=======
-        <a class="link is-info" href="login">Login</a>
->>>>>>> 0c2a2981ccfcf29f62b65f02ee5bdf64db70fa1b
       </p>
       <p class="level-item has-text-centered">
         <a class="link is-info">Contact</a>

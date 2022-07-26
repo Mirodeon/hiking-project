a527443e5e09b932f67510a7902ffd3ee177a865
unknown <luuduc34@hotmail.com>
2022-07-26 18:27:30 +0200

    fix hiking project title in header

--- src/app/views/header.php
+++ src/app/views/header.php
@@ -1,8 +1,12 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap');
+</style>
+
 <nav class="navbar is-info">
   <div class="container">
     <div class="navbar-brand">
-      <a href="home" class="navbar-item" style="font-weight: bold;">
-        THE HIKING PROJECT
+      <a href="home" class="navbar-item is-size-5" style="font-family: 'Mochiy Pop One', sans-serif;">
+        The hiking project
       </a>
       <span class="navbar-burger burger" data-target="navMenu">
         <span></span>
@@ -19,10 +23,12 @@
       <div class="navbar-end">
         <div class="navbar-item">
           <div class="buttons">
-            <a class="button is-primary is-small js-modal-trigger" href="register" > <!-- data-target="modal-signup" -->
+            <a class="button is-primary is-small js-modal-trigger" href="register">
+              <!-- data-target="modal-signup" -->
               <strong>Sign up</strong>
             </a>
-            <a class="button is-light is-small js-modal-trigger" href="login" > <!-- data-target="modal-login" -->
+            <a class="button is-light is-small js-modal-trigger" href="login">
+              <!-- data-target="modal-login" -->
               Login
             </a>
           </div>

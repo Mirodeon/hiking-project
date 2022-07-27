1b3c4a26b7e61b0875a355d9aa4abb64d5e58b19
unknown <luuduc34@hotmail.com>
2022-07-27 15:11:49 +0200

    User profile page updated for update

--- src/app/views/profile.php
+++ src/app/views/profile.php
@@ -5,45 +5,95 @@
 <?php $title = "Profile - " . $_SESSION["user"]["login"]; ?>
 <?php require "parts/head.php"; ?>
 <?php include 'header.php'; ?>
-
+</br>
 <section class="container">
     <div class="column is-half is-offset-one-quarter has-text-centered">
         <article class="panel is-primary">
-            <p class="panel-heading">User profile</p>
-            <div class="panel-block"></div>
-            <div class="field">
-                <span class="icon is-small is-left">
-                    <i class="fa fa-user-o"></i>
-                </span>
-                firstname: <?php echo $_SESSION["user"]["firstname"]; ?>
-            </div>
-            <div class="field">
-                <span class="icon is-small is-left">
-                    <i class="fa fa-user-o"></i>
-                </span>
-                lastname: <?php echo $_SESSION["user"]["lastname"]; ?>
-            </div>
-            <div class="field">
-                <span class="icon is-small is-left">
-                    <i class="fa fa-sign-in"></i>
-                </span>
-                login: <?php echo $_SESSION["user"]["login"]; ?>
-            </div>
-            <div class="field">
-                <span class="icon is-small is-left">
-                    <i class="fa fa-envelope-o"></i>
-                </span>
-                email: <?php echo $_SESSION["user"]["email"]; ?>
-            </div>
-            <div class="is-centered">
-            <button class="button is-light">Update</button>
-            <button class="button is-primary">Submit</button>
-            </div>
-            <div class="panel-block"></div>
+            <p class="panel-heading">User profile</p></br>
+            <div class="blabla">
+                <div class="field">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-user-o"></i>
+                    </span>
+                    firstname: <?php echo $_SESSION["user"]["firstname"]; ?>
+                </div>
+                <div class="field">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-user-o"></i>
+                    </span>
+                    lastname: <?php echo $_SESSION["user"]["lastname"]; ?>
+                </div>
+                <div class="field">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-sign-in"></i>
+                    </span>
+                    login: <?php echo $_SESSION["user"]["login"]; ?>
+                </div>
+                <div class="field">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-envelope-o"></i>
+                    </span>
+                    email: <?php echo $_SESSION["user"]["email"]; ?>
+                </div></br>
+                <button class="button is-light">Update</button>
+            </div></br>
+            <!-- <div class="blabla column is-half is-offset-one-quarter">
+                <form method="post" class="" action="signup">
+                    <div class="field">
+                        <label for="firstname" class="label is-small">Firstname</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="text" class="input is-small" placeholder="<?php echo $_SESSION["user"]["firstname"]; ?>" name="firstname">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-user-o"></i>
+                        </span>
+                    </div>
+                    <div class="field">
+                        <label for="lastname" class="label is-small">Lastname</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="text" class="input is-small" placeholder="<?php echo $_SESSION["user"]["lastname"]; ?>" name="lastname">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-user-o"></i>
+                        </span>
+                    </div>
+                    <div class="field">
+                        <label for="email" class="label is-small">Email</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="email" class="input is-small" placeholder="<?php echo $_SESSION["user"]["email"]; ?>" name="email">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-envelope-o"></i>
+                        </span>
+                    </div>
+                    <div class="field">
+                        <label for="login" class="label is-small">Login</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="text" class="input is-small" placeholder="<?php echo $_SESSION["user"]["login"]; ?>" name="login">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-sign-in"></i>
+                        </span>
+                    </div>
+                    <div class="field">
+                        <label for="pass" class="label is-small">New password</label>
+                    </div>
+                    <div class="control has-icons-left">
+                        <input type="text" class="input is-small" placeholder="New password" name="pass">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                    </div></br>
+                    <button class="button is-primary">Submit</button>
+                    <p class="label is-small has-text-danger"><?= (isset($_SESSION['error'])) ? $_SESSION['error'] : "" ?></p>
+                </form>
+                </br>
+            </div> -->
         </article>
     </div>
-</section>
 
+</section>
+</br>
 <?php include "footer.php"; ?>
 
 

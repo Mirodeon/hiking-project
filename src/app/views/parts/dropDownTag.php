bd9233584db8d3037bfed7ffe1a8cb125dde1fdb
unknown <luuduc34@hotmail.com>
2022-08-01 16:50:36 +0200

    add difficulty to dropdownTag

--- src/app/views/parts/dropDownTag.php
+++ src/app/views/parts/dropDownTag.php
@@ -1,35 +1,65 @@
 <div class="dropdown is-hoverable">
-      <div class="dropdown-trigger">
-        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-          <span>Tags</span>
-          <span class="icon is-small">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </button>
-      </div>
-      <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-        <div class="dropdown-content">
-          <div class="dropdown-item">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+      <span>Tags</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+    <div class="dropdown-content">
+      <div class="dropdown-item">
 
-            <a href="#" class="dropdown-item">
-              hill
-            </a>
-            <a href="#" class="dropdown-item">
-              mountain
-            </a>
-            <a href="#" class="dropdown-item">
-              grass
-            </a>
-            <a href="#" class="dropdown-item">
-              lake
-            </a>
-            <a href="#" class="dropdown-item">
-              sea
-            </a>
-            <a href="#" class="dropdown-item">
-              snow
-            </a>
-          </div>
-        </div>
+        <a href="#" class="dropdown-item">
+          hill
+        </a>
+        <a href="#" class="dropdown-item">
+          mountain
+        </a>
+        <a href="#" class="dropdown-item">
+          grass
+        </a>
+        <a href="#" class="dropdown-item">
+          lake
+        </a>
+        <a href="#" class="dropdown-item">
+          sea
+        </a>
+        <a href="#" class="dropdown-item">
+          snow
+        </a>
       </div>
     </div>
+  </div>
+</div>
+<div class="dropdown is-hoverable">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+      <span>Difficulty</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+    <div class="dropdown-content">
+      <div class="dropdown-item">
+
+        <a href="#" class="dropdown-item">
+          easy
+        </a>
+        <a href="#" class="dropdown-item">
+          normal
+        </a>
+        <a href="#" class="dropdown-item">
+          hard
+        </a>
+        <a href="#" class="dropdown-item">
+          extreme
+        </a>
+        
+      </div>
+    </div>
+  </div>
+</div>

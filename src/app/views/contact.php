5ffdf8be371123dd54cd89d9fb1cbf64e949dd57
unknown <luuduc34@hotmail.com>
2022-08-03 18:43:42 +0200

    add about us in contact page

--- src/app/views/contact.php
+++ src/app/views/contact.php
@@ -6,83 +6,155 @@
 } ?>
 <?php require "parts/head.php"; ?>
 <?php include 'header.php'; ?>
-
+<style>
+    .box {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
 <div class="hero-body">
     <section class="container flex center">
         <div class="columns is-centered">
-            <div class="column is-half">
+            <div class="column">
+                <article class="panel is-link">
+                    <p class="panel-heading has-text-centered">About us</p>
+                    <div class="column">
+                        <p>For millions of people, Elderberry is the key to the great outdoors. From intrepid explorers and experienced off-roaders, to those who just bought their first road bike, our technology allows everybody to better find, plan and live authentic outdoor experiences, we built Elderberry for this very reason</p><br>
+                        <article class="message is-success">
+                            <div class="message-body">
+                                <p><em>"I believe that every day spent outside exploring is a valuable day; more valuable than anything you can buy. At the end of your life you will look back not on the things you owned but on the things you experienced—we help people experience more"</em></p>
+                                <p style="text-align: right;">Mehdi HABARU Co-founder, Web developper</p>
+                            </div>
+                        </article>
+                    </div>
+                </article>
+                <div class="box">
+                    <div id="donate-button-container">
+                        <div id="donate-button"></div>
+                        <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+                        <script>
+                            PayPal.Donation.Button({
+                                env: 'production',
+                                hosted_button_id: 'KUXTHQWZDTXCW',
+                                image: {
+                                    src: 'https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif',
+                                    alt: 'Donate with PayPal button',
+                                    title: 'PayPal - The safer, easier way to pay online!',
+                                }
+                            }).render('#donate-button');
+                        </script>
+                    </div>
+
+                    <p><strong>DONATE - SOCIAL</strong></p>
+                    <p><a href=""><span class="icon-text">
+                                <span class="icon">
+                                    <i class="fa-brands fa-twitter fa-lg"></i>
+                                </span>
+                            </span></a>
+                        <a href=""><span class="icon-text">
+                                <span class="icon">
+                                    <i class="fa-brands fa-facebook fa-lg"></i>
+                                </span>
+                            </span></a>
+                        <a href=""><span class="icon-text">
+                                <span class="icon">
+                                    <i class="fa-brands fa-instagram fa-lg"></i>
+                                </span>
+                            </span></a>
+                    </p>
+                </div>
+                <!-- <div>
+                    <article class="panel is-info">
+                        <p class="panel-heading has-text-centered">Follow us</p>
+                        <div class="column">
+                            <a href=""><span class="icon-text">
+                                <span class="icon">
+                                <i class="fa-brands fa-twitter fa-lg"></i>
+                                </span>
+                            </span></a>
+                            <a href=""><span class="icon-text">
+                                <span class="icon">
+                                <i class="fa-brands fa-facebook fa-lg"></i>
+                                </span>
+                            </span></a>
+                            <a href=""><span class="icon-text">
+                                <span class="icon">
+                                <i class="fa-brands fa-instagram fa-lg"></i>
+                                </span>
+                            </span></a>
+                        </div>
+                    </article>
+                </div> -->
+            </div>
+
+            <div class="column is-one-third">
                 <article class="panel is-success">
                     <p class="panel-heading has-text-centered">Contact us</p>
-                    <p>ABOUT US
-                        Our goal is to bring you closer to places and people that would otherwise seem foreign by providing guides to studying languages, understanding culture, and living in unfamiliar places.</p>
-                </article>
-            </div>
-            <article class="panel is-success">
-                <p class="panel-heading has-text-centered">Contact us</p>
-                <div class="column" style="background-image: url('./img/mail_image.jpg'); background-repeat: no-repeat; background-size:cover; height:150px"></div>
-                <div class="column">
-                    <form action="sendContact" method="post">
-                        <div class="field">
-                            <label class="label">Firstname</label>
-                            <div class="control has-icons-left has-icons-right">
-                                <input class="input" type="text" placeholder="Firstname" name="firstname">
-                                <span class="icon is-small is-left">
-                                    <i class="fas fa-user"></i>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label">Lastname</label>
-                            <div class="control has-icons-left has-icons-right">
-                                <input class="input" type="text" placeholder="Lastname" name="lastname">
-                                <span class="icon is-small is-left">
-                                    <i class="fas fa-user"></i>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label">Email</label>
-                            <div class="control has-icons-left has-icons-right">
-                                <input class="input" type="email" placeholder="Email adress" name="email" required>
-                                <span class="icon is-small is-left">
-                                    <i class="fas fa-envelope"></i>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label">Subject</label>
-                            <div class="control">
-                                <div class="select">
-                                    <select name="subject">
-                                        <option>Select a subject</option>
-                                        <option value="User">Users</option>
-                                        <option value="Hike">Hikes</option>
-                                        <option value="Website">Website</option>
-                                    </select>
+                    <div class="column" style="background-image: url('./img/mail_image.jpg'); background-repeat: no-repeat; background-size:cover; height:150px"></div>
+                    <div class="column">
+                        <form action="sendContact" method="post">
+                            <div class="field">
+                                <label class="label">Firstname</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input" type="text" placeholder="Firstname" name="firstname">
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="field">
-                            <label class="label">Message</label>
-                            <div class="control">
-                                <textarea class="textarea" placeholder="Your message" name="message" spellcheck="false"></textarea>
+                            <div class="field">
+                                <label class="label">Lastname</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input" type="text" placeholder="Lastname" name="lastname">
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="field is-grouped">
-                            <div class="control">
-                                <button class="button is-link" type="submit">Submit</button>
+
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input" type="email" placeholder="Email adress" name="email" required>
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-envelope"></i>
+                                    </span>
+                                </div>
                             </div>
-                            <div class="control">
-                                <button class="button is-link is-light" type="reset">Reset</button>
+
+                            <div class="field">
+                                <label class="label">Subject</label>
+                                <div class="control">
+                                    <div class="select">
+                                        <select name="subject">
+                                            <option>Select a subject</option>
+                                            <option value="User">Users</option>
+                                            <option value="Hike">Hikes</option>
+                                            <option value="Website">Website</option>
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </div>
-            </article>
+
+                            <div class="field">
+                                <label class="label">Message</label>
+                                <div class="control">
+                                    <textarea class="textarea" placeholder="Your message" name="message" spellcheck="false"></textarea>
+                                </div>
+                            </div>
+                            <div class="field is-grouped">
+                                <div class="control">
+                                    <button class="button is-link" type="submit">Submit</button>
+                                </div>
+                                <div class="control">
+                                    <button class="button is-link is-light" type="reset">Reset</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </article>
+            </div>
         </div>
     </section>
 </div>
